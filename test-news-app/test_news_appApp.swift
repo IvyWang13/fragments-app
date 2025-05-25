4965bc40aa79962486875d0014e05ff24a870e5c
IvyWang13 <ivywang0531@gmail.com>
2025-05-25 13:54:46 +0800

    Initial Commit

---
+++ test-news-app/test_news_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_news_appApp.swift
+//  test-news-app
+//
+//  Created by Ivy Wang on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct test_news_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test-news-appTests/test_news_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_news_appTests.swift
+//  test-news-appTests
+//
+//  Created by Ivy Wang on 5/25/25.
+//
+
+import Testing
+@testable import test_news_app
+
+struct test_news_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
